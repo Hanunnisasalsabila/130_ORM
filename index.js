68d9a9ae4d7dbbe9e4df360736b4d6b4e3e3b280
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-10-29 14:37:38 +0700

    membuat POST(data komik)

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const db = require('./models');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+db.sequelize.sync()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server started on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.post('/komik', async (req, res) => {
+    const data = req.body;
+    try {
+        const komik = await db.Komik.create(data);
+        res.send(komik);
+    } 
+    catch (error) {
+        res.status(500).send({message: error.message}); 
+    }
+});
